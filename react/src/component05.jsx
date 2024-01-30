fc2df94fe11949cf512037610a20f22e5c3a3fb3
Vivek Kumar <115945472+vivekBoii@users.noreply.github.com>
2024-01-31 02:40:18 +0530

    Generated Code

---
+++ react/src/component05.jsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+
+
+export function component05(props) {
+
+
+
+return (
+<>
+      <div className='component05'>
+        <div className='Saly-38'
+  
+  style={{
+    
+width: 'full',
+minHeight: '100vh',
+visibility: true,
+position: 'relative',
+opacity: 1.0,
+overflow: 'visible',
+borderRadius: '0px',
+boxSizing: 'border-box',
+  }}>
+  <img style={{
+left: '0%',
+width: '100%',
+top: '0%',
+height: '100%',
+visibility: true,
+position: 'absolute',
+overflow: 'visible',
+backgroundSize: 'cover',
+borderRadius: '0px',
+boxSizing: 'border-box'}}
+src='https://timon-assets-dev.s3.ap-south-1.amazonaws.com/project/e97cb38e-5673-4b34-b70e-f97f6f1e0be8/asset/127795f361b344e7c9c2513005ab3f00248e3faa?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240127T054955Z&X-Amz-SignedHeaders=host&X-Amz-Expires=30000&X-Amz-Credential=AKIA3TU5OEBRBYBNQRFE%2F20240127%2Fap-south-1%2Fs3%2Faws4_request&X-Amz-Signature=f7e140ba05447b20cf243205073ed264c745501b46fb0b6b84ffb134da314e73' 
+ />
+
+</div>
+
+      </div>
+</>
+
+);
+}
